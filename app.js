fa8922bb611872279056019b3634aa38107bed3e
Karl <toom.karl42@gmail.com>
2023-04-24 14:44:59 +0300

    return opposite boolean

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-/*function sumOfTwoNumbers(num1, num2) {
-  num1 + num2 > 100 ? console.log(true) : console.log(false);
+function oppositeBoolean(bool) {
+  return !bool
 }
-sumOfTwoNumbers(50, 40);
-*/
-function lessThanOrEqualToZero(num) {
-    return num <=0
-  }
-  console.log(lessThanOrEqualToZero(-1))
-  
+console.log(oppositeBoolean(true));
